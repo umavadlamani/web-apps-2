c922eff71555dc6bd2a0f88d69f1f6ce248ebd40
uma vadlamani <umasai.vadlamani@kellogg.northwestern.edu>
2024-02-20 15:23:20 +0000

    Web apps 2 done

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -1,7 +1,22 @@
 class BitcoinsController < ApplicationController
 
   def index
-    # add backend code
+  # add backend code
+ 
+  @bitcoin = params["amount"]
+
+  #connect to coindesk API - do not change
+  require "net/http"
+  require "json"
+  url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  bitcoin_data = JSON.parse(response)
+  #-----
+
+  @usd_rate = bitcoin_data["bpi"]["USD"]["rate_float"]
+  @usd_value = @usd_rate * @bitcoin.to_f
+
     # render bitcoins/index view
   end
 
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,24 +2,33 @@ class CompaniesController < ApplicationController
 
   def index
     # find all Company rows
+    @companies = Company.all
     # render companies/index view
   end
 
-  # def show
-  #   # find a Company
-  #   # render companies/show view with details about Company
-  # end
+  def show
+     #find a Company
+     @company = Company.find_by({"id" => params["id"]})
+     #render companies/show view with details about Company
+  end
 
-  # def new
-  #   # render view with new Company form
-  # end
+  def new
+     # render view with new Company form
+  end
 
-  # def create
-  #   # start with a new Company
-  #   # assign user-entered form data to Company's columns
-  #   # save Company row
-  #   # redirect user
-  # end
+  def create
+    # start with a new Company
+    @company = Company.new
+    # assign user-entered form data to Company's columns
+    @company["name"] = params["name"]
+    @company["city"] = params["city"]
+    @company["state"] = params["state"]
+  
+    # save Company row
+    @company.save
+    # redirect user
+    redirect_to "/companies"
+  end
 
   # def edit
   #   # find a Company
