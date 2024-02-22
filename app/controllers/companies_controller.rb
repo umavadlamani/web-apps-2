317fb01e68a96010e875c9762e5dd2f48d5f40af
uma vadlamani <umasai.vadlamani@kellogg.northwestern.edu>
2024-02-22 00:52:09 +0000

    Need to finish zoom

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,6 +9,7 @@ class CompaniesController < ApplicationController
   def show
      #find a Company
      @company = Company.find_by({"id" => params["id"]})
+     @contacts = Contact.where({"company_id" => @company["id"]})
      #render companies/show view with details about Company
   end
 
